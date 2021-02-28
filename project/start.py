bcee42cb32ea22a98e4764e613a7e5d729787453
Yi Yang <yi.yang6@mcgill.ca>
2021-02-27 22:16:55 -0500

    The first download

---
+++ project/start.py
@@ -0,0 +1,39 @@
+from manimlib import *
+
+class TextExample(Scene):
+    def construct(self):
+        text = Text("Here is a text", font="Consolas", font_size=90)
+        difference = Text(
+            """
+            The most important difference between Text and TexText is that\n
+            you can change the font more easily, but can't use the LaTeX grammar
+            """,
+            font="Arial", font_size=24,
+            t2c={"Text": BLUE, "TexText": BLUE, "LaTeX": ORANGE}
+        )
+        text2 = Tex(r'\sum_{1}^{n}\sin(x_i)')
+        VGroup(text, difference).arrange(DOWN, buff=1)
+        self.play(Write(text))
+        self.play(Write(text2))
+        self.play(FadeIn(difference, UP))
+        self.wait(3)
+
+        fonts = Text(
+            "And you can also set the font according to different words",
+            font="Arial",
+            t2f={"font": "Consolas", "words": "Consolas"},
+            t2c={"font": BLUE, "words": GREEN}
+        )
+        slant = Text(
+            "And the same as slant and weight",
+            font="Consolas",
+            t2s={"slant": ITALIC},
+            t2w={"weight": BOLD},
+            t2c={"slant": ORANGE, "weight": RED}
+        )
+        VGroup(fonts, slant).arrange(DOWN, buff=0.8)
+        self.play(FadeOut(text), FadeOut(difference, shift=DOWN))
+        self.play(Write(fonts))
+        self.wait()
+        self.play(Write(slant))
+        self.wait()
